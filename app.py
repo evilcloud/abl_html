e38487f65a7c94fa03dfd22d71661a6f9d5a7757
GK <kg@GKs-MacBook-Pro.local>
2022-07-18 11:11:04 +0200

    deploy

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from app.main import app
+from pydoc import render_doc
+from flask import Flask, render_template
+import mongo_data
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    total = mongo_data.get_wallets_total()
+    return render_template("index.html", total=f"{total:,}")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(app.run(threaded=True, port=5000))
+    # app.run(host="127.0.0.1", port=8080, debug=True)
